c3dde4ab9a1193f9010d3d920f2fe06e9a9d4f42
mauroapjr <mauroapjr@outlook.com>
2023-01-23 16:03:33 -0500

    Passed data and included a tagline

--- express_server.js
+++ express_server.js
@@ -10,7 +10,22 @@ const urlDatabase = {
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("pages/index");
+  var mascots = [
+    { name: "Sammy", organization: "DigitalOcean", birth_year: 2012 },
+    { name: "Tux", organization: "Linux", birth_year: 1996 },
+    { name: "Moby Dock", organization: "Docker", birth_year: 2013 },
+  ];
+  var tagline =
+    "No programming concept is complete without a cute animal mascot.";
+
+  res.render("pages/index", {
+    mascots: mascots,
+    tagline: tagline,
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("pages/about");
 });
 
 app.get("/urls.json", (req, res) => {
